b4c264f1bc5b9f0c36a61d4c02e33e5e9dfe4f4b
WilsonCruzz <wilson860731@gmail.com>
2025-03-19 22:08:30 +0800

    week 6 done

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.assignment2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,30 +10,43 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.assignment2.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
     MainActivityViewModel viewModel;
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main);
 
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
-        TextView resultsTv = findViewById(R.id.resultsTv);
-        resultsTv.setText(String.valueOf(viewModel.getNumber()));
+        viewModel.getNumber().observe(this,getData->{
+            Log.i("tag","observe trigger");
 
-        Button addOneBtn = findViewById(R.id.add1Btn);
-        addOneBtn.setOnClickListener(new View.OnClickListener(){
+
+            //TextView resultsTv = findViewById(R.id.resultsTv);
+            String strData = Integer.toString(getData);
+            binding.resultsTv.setText(strData);
+
+        });
+
+        //Button addOneBtn = findViewById(R.id.add1Btn);
+        binding.add1Btn.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view){
 
                 viewModel.addOne();
-                resultsTv.setText(String.valueOf(viewModel.getNumber()));
-
             }
         });
     }
--- app/src/main/java/com/example/assignment2/MainActivityViewModel.java
+++ app/src/main/java/com/example/assignment2/MainActivityViewModel.java
@@ -1,19 +1,25 @@
 package com.example.assignment2;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainActivityViewModel extends ViewModel {
 
     int counter = 0;
+    private MutableLiveData<Integer> myValue;
 
     public MainActivityViewModel(){
+        myValue = new MutableLiveData<>();
+        myValue.setValue(counter);
 
     }
 
     public void addOne(){
-        ++counter;
+        myValue.setValue(++counter);
+
     }
-    public int getNumber(){
-        return counter;
+    public LiveData<Integer> getNumber(){
+        return myValue;
     }
 }
